f88ecbc5636545359d8a4636836b5bd346a7c3bf
Daniel Colson <danieljamescolson@gmail.com>
2022-12-04 01:02:30 -0500

    Add day4 nonsense solution

---
+++ day4/nonsense_solution.rb
@@ -0,0 +1,41 @@
+  r=$data.
+split(
+  0xa.
+chr
+  ).
+map {
+  |l|l.
+split(
+  0x2c.
+chr
+  ).
+map {
+  _1.
+split(
+  0x2d.
+chr
+  ).
+map(
+ &("$=&]?:0``\n".
+unpack(
+  "u")[0].
+to_sym
+  ))}.
+map {
+  _1.._2}};p r.
+count {
+  _1.
+cover?(
+  _2)||_2.
+cover?(_1)
+  };p r.
+count {
+  _1.
+end >=
+  _2.
+begin &&
+  _2.
+end >=
+  _1.
+begin
+  }
