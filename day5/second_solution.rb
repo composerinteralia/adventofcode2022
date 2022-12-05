f7f2da7ab9c6409e0cb65b609144f02a39695519
Daniel Colson <danieljamescolson@gmail.com>
2022-12-05 11:05:05 -0500

    Add shorter day5 solution

---
+++ day5/second_solution.rb
@@ -0,0 +1,25 @@
+$raw_stacks,$insns = $data.split("\n\n")
+
+def run
+  build_stacks.tap do |stacks|
+    follow_insns { |count, from, to| yield count, stacks[to -1], stacks[from -1] }
+    puts stacks.map { _1.last }.join
+  end
+end
+
+def build_stacks
+ $raw_stacks.split("\n")[0...-1]
+   .map { parse_stack_line(_1) }
+   .reverse.transpose.map(&:compact)
+end
+
+def parse_stack_line(line)
+  line.chars.each_slice(4).map(&:join).map { _1[1] unless _1[1].strip.empty? }
+end
+
+def follow_insns
+  $insns.split("\n").each { |insn| yield *insn.scan(/\d+/).map(&:to_i) }
+end
+
+run { |count, to, from| count.times { to << from.pop } }
+run { |count, to, from| to.push(*from.pop(count)) }
